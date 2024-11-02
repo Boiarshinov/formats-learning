80681767699703eb6aac3a87924738ce41a7f167
boiarshinov <artem.borya@gmail.com>
2024-11-02 16:49:52 +0300

    Set UTF-8 encoding everywhere

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,5 +19,12 @@ subprojects {
     tasks.test {
         useJUnitPlatform()
     }
+
+    tasks {
+        withType<JavaCompile>().configureEach { options.encoding = "UTF-8" }
+        withType<JavaExec>().configureEach { defaultCharacterEncoding = "UTF-8" }
+        withType<Javadoc>().configureEach { options.encoding = "UTF-8" }
+        withType<Test>().configureEach { defaultCharacterEncoding = "UTF-8" }
+    }
 }
 
